917a0324dd0ce5e824fa50d06d70d48fa76f7365
Orange27-v <83568592+Orange27-v@users.noreply.github.com>
2024-05-07 14:47:52 +0100

    Add files via upload

--- src/components/assets.js
+++ src/components/assets.js
@@ -2,31 +2,31 @@
 
 
 
-export const Logo = "./src/assets/logo/logo.png";
-export const SKBlackWhite = "./src/assets/person/57495.png";
-export const personPicture = "./src/assets/person/85886.png";
-export const collaborattionPicture = "./src/assets/person/colaborations.jpeg";
-export const shippingImage = "./src/assets/background/bg-2.png";
+export const Logo = "../public/assets/logo/logo.png";
+export const SKBlackWhite = "../public/assets/person/57495.png";
+export const personPicture = "../public/assets/person/85886.png";
+export const collaborattionPicture = "../public/assets/person/colaborations.jpeg";
+export const shippingImage = "../public/assets/background/bg-2.png";
 
-export const Atraining = "./src/assets/person/training.jpeg";
-export const  Btraining = "./src/assets/person/training-2.jpeg";
-export const Ctraining = "./src/assets/person/training-3.jpeg";
-export const Dtraining = "./src/assets/person/training-4.jpeg";
-export const gvestbanner = "./src/assets/person/gvest-banner.jpeg";
-export const certified = "./src/assets/person/certified.jpeg";
+export const Atraining = "../public/assets/person/training.jpeg";
+export const  Btraining = "../public/assets/person/training-2.jpeg";
+export const Ctraining = "../public/assets/person/training-3.jpeg";
+export const Dtraining = "../public/assets/person/training-4.jpeg";
+export const gvestbanner = "../public/assets/person/gvest-banner.jpeg";
+export const certified = "../public/assets/person/certified.jpeg";
 
 // export const Arrow = "../assets/background/clipart/arrow.png";
 
-// export const SKBlackWhite = './src/assets/person/57495.png';
-// export const personPicture = './src/assets/person/85886.png';
+// export const SKBlackWhite = '../public/assets/person/57495.png';
+// export const personPicture = '../public/assets/person/85886.png';
 
 export const brands = {
-  land: "./src/assets/our-brands/gtxland.png",
-  holding: "./src/assets/our-brands/Gtxholding.png",
-  university: "./src/assets/our-brands/university.png",
-  consulting: "./src/assets/our-brands/saconsulting.png",
-  investment: "./src/assets/our-brands/gvest.png",
-  foundation: "./src/assets/our-brands/foundation.png",
+  land: "../public/assets/our-brands/gtxland.png",
+  holding: "../public/assets/our-brands/Gtxholding.png",
+  university: "../public/assets/our-brands/university.png",
+  consulting: "../public/assets/our-brands/saconsulting.png",
+  investment: "../public/assets/our-brands/gvest.png",
+  foundation: "../public/assets/our-brands/foundation.png",
 };
 
 
