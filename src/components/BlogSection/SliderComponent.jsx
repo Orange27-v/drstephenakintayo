ff3b75235f55587f46a1342e7bc7f6c2a4cac408
Orange27-v <83568592+Orange27-v@users.noreply.github.com>
2024-05-07 13:56:14 +0100

    Add files via upload

--- src/components/BlogSection/SliderComponent.jsx
+++ src/components/BlogSection/SliderComponent.jsx
@@ -67,9 +67,41 @@ function CustomArrows() {
     slidesToShow: 2,
     slidesToScroll: 1,
     infinite: true,
+    autoplay: true,
     speed: 2000,
+    autoplaySpeed: 1000,
+    pauseOnHover: true,
+    cssEase: "linear",
+    adaptiveHeight: true,
     nextArrow: <SampleNextArrow />,
     prevArrow: <SamplePrevArrow />,
+
+    responsive: [
+      {
+        breakpoint: 1024,
+        settings: {
+          slidesToShow: 3,
+          slidesToScroll: 3,
+          infinite: true,
+          dots: true,
+        },
+      },
+      {
+        breakpoint: 600,
+        settings: {
+          slidesToShow: 2,
+          slidesToScroll: 2,
+          initialSlide: 2,
+        },
+      },
+      {
+        breakpoint: 480,
+        settings: {
+          slidesToShow: 1,
+          slidesToScroll: 1,
+        },
+      },
+    ],
   };
   return (
     <div
@@ -85,160 +117,60 @@ function CustomArrows() {
             <h2 className="space-30">Some Recent Events and Trainings...</h2>
           </div>
 
-          <div className="slider-container slider-container-custom ">
+          <div className="slider-container slider-container-custom">
             <Slider {...settings}>
               <div>
-                <div className="d-flex">
+                <div className="row">
                   <div className="col-full-width">
-                    <div className="feature-colabroations">
-                      <div
-                        style={{ display: "flex", justifyContent: "center" }}
-                      >
+                    <div className="feature-collaborations">
+                     
                         <FacebookEmbed
-                          url="https://web.facebook.com/drsakintayo/posts/pfbid02e7t5PbgRdaGfKc7xnre2SH387SvDPvYop3c6bb2y81tdNWXZuShB1Rfqo4tqiyrGl"
-                          width={450}
-                          height={550}
+                          url="https://web.facebook.com/photo/?fbid=1001641301330615&set=pb.100044542624764.-2207520000"
+                          className="feature-post"
                         />
-                      </div>
+                     
                     </div>
                   </div>
                 </div>
               </div>
               <div>
-                <div className="d-flex">
+                <div className="row">
                   <div className="col-full-width">
-                    <div className="feature-colabroations">
-                      <div
-                        style={{ display: "flex", justifyContent: "center" }}
-                      >
-                        <div
-                          style={{ display: "flex", justifyContent: "center" }}
-                        >
-                          <InstagramEmbed
-                            url="https://www.instagram.com/p/C6eOQ5irIvk/?fbclid=IwZXh0bgNhZW0CMTAAAR0Xpg-aPt-yECMeG0mi66w1IcCzwOhtZ1eMjGHa2G1ovG41snbnM0diTyk_aem_AUSAuBEN05dY35h29Z2URKKcEKA703aUPzEt8WVFRJb7l-3NtF8StSfEYv9VZ18Ceh8sK9G3o8ZS9swgbDR6JOHS&img_index=2"
-                            width={450}
-                            height={550}
-                            captioned />
-                        </div>
-                      </div>
+                    <div className="feature-collaborations">
+                    <FacebookEmbed
+                          url="https://web.facebook.com/photo/?fbid=999188408242571&set=pb.100044542624764.-2207520000"
+                          className="feature-post"
+                        />
+                     
+                     
                     </div>
                   </div>
                 </div>
               </div>
               <div>
-                <div className="d-flex">
-                  <div className="col-two-width">
-                    <div className="feature-colabroations">
-                      <div className="featured-image">
-                        <img src={Ctraining} alt="" />
-                      </div>
-
-                      <div className="feature-link d-flex">
-                        <div className="featured-dash"></div>
-                        <div className="featured-info">
-                          <p>
-                            {" "}
-                            Lorem ipsum dolor sit. Lorem ipsum dolor sit amet
-                            consectetur adipisicing.{" "}
-                          </p>
-                        </div>
-                      </div>
-                    </div>
-                  </div>
-                  <div className="col-two-width">
-                    <div className="feature-colabroations">
-                      <div className="feature-section">
-                        <div className="feature-title ">
-                          <h2>EFFECTIVE REALESTATE DEVELOPMENT</h2>
-                        </div>
-                        <p>
-                          Another edition is here! The CEOs Leadership Retreat
-                          is happening live in Ghana! The venue is Kempinski
-                          Hotel, Ghana. The date is from 16th - 20th May, 2024.
-                          It's going to be 10X of the previous editions, which
-                          you cannot afford to miss.
-                        </p>
-
-                        <p>
-                          Akintayo urges Nigerian entrepreneurs to create value
-                          amid the economic challenges facing the country. It
-                          was 5 days of FIRE! 🔥🔥 The truth was laid bare.
-                          Mediocrity was attacked, and the spirit of excellence
-                          was birthed.
-                        </p>
-
-                        <p>
-                          Akintayo urges Nigerian entrepreneurs to create value
-                          amid the economic challenges facing the country. What
-                          I post on social media is just a fraction of what is
-                          in me to pour out. If you want to move to the next
-                          level in your business, move close.
-                        </p>
-                        <div className="feature-btn">
-                          <button className="btn btn-lg btn-blue">
-                            Get More Information
-                          </button>
-                        </div>
-                      </div>
+                <div className="row">
+                  <div className="col-full-width">
+                    <div className="feature-collaborations">
+                    <FacebookEmbed
+                          url="https://web.facebook.com/drsakintayo/posts/pfbid02mEhQ8d8vDzQ6piCmdVdV2kRMfg74LT8AncyPSnabSZdDoymhT9cY84FAFoVoXar7l"
+                          className="feature-post"
+                        />
+                     
+                     
                     </div>
                   </div>
                 </div>
               </div>
               <div>
-                <div className="d-flex">
-                  <div className="col-two-width">
-                    <div className="feature-colabroations">
-                      <div className="featured-image">
-                        <img src={gvestbanner} alt="" />
-                      </div>
-
-                      <div className="feature-link d-flex">
-                        <div className="featured-dash"></div>
-                        <div className="featured-info">
-                          <p>
-                            {" "}
-                            Lorem ipsum dolor sit. Lorem ipsum dolor sit amet
-                            consectetur adipisicing.{" "}
-                          </p>
-                        </div>
-                      </div>
-                    </div>
-                  </div>
-                  <div className="col-two-width">
-                    <div className="feature-colabroations">
-                      <div className="feature-section">
-                        <div className="feature-title ">
-                          <h2>EFFECTIVE REALESTATE DEVELOPMENT</h2>
-                        </div>
-                        <p>
-                          Another edition is here! The CEOs Leadership Retreat
-                          is happening live in Ghana! The venue is Kempinski
-                          Hotel, Ghana. The date is from 16th - 20th May, 2024.
-                          It's going to be 10X of the previous editions, which
-                          you cannot afford to miss.
-                        </p>
-
-                        <p>
-                          Akintayo urges Nigerian entrepreneurs to create value
-                          amid the economic challenges facing the country. It
-                          was 5 days of FIRE! 🔥🔥 The truth was laid bare.
-                          Mediocrity was attacked, and the spirit of excellence
-                          was birthed.
-                        </p>
-
-                        <p>
-                          Akintayo urges Nigerian entrepreneurs to create value
-                          amid the economic challenges facing the country. What
-                          I post on social media is just a fraction of what is
-                          in me to pour out. If you want to move to the next
-                          level in your business, move close.
-                        </p>
-                        <div className="feature-btn">
-                          <button className="btn btn-lg btn-blue">
-                            Get More Information
-                          </button>
-                        </div>
-                      </div>
+                <div className="row">
+                  <div className="col-full-width">
+                    <div className="feature-collaborations">
+                    <FacebookEmbed
+                          url="https://web.facebook.com/drsakintayo/posts/pfbid0r9JKkmEf9egQVxHYFb2c79wSxBdZ6caQyF1pRZVoxV3pjrUr2zAoXZKiWhkJ8iiFl"
+                          className="feature-post"
+                        />
+                     
+                     
                     </div>
                   </div>
                 </div>
